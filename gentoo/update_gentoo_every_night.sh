64ef570e2b2a9a3d4ef1fd775db30baf6798ca2a
Sergey Galkin <sergey.gals@gmail.com>
2020-01-08 12:51:08 +0400

    Change update gentoo way

--- gentoo/update_gentoo_every_night.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash - 
-tmux new-window -t emerge -d  'eix-sync ; layman -S ; eix-update; emerge -vtuN --deep --keep-going world; emerge @preserved-rebuild; bash' 2> /dev/null || tmux new-session -s emerge -d 'eix-sync ; layman -S ; eix-update; emerge -vtuN --deep --keep-going world; emerge @preserved-rebuild; bash'
